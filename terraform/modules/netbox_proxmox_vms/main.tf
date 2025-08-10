f47efdd3d307a2900f3fe7dd5bb38fc1803ad514
rene <rene.ruprecht.396@gmail.com>
2025-08-10 15:26:53 +0200

    refactor: netbox_proxmox_vms replace foreach with count

--- terraform/modules/netbox_proxmox_vms/main.tf
+++ terraform/modules/netbox_proxmox_vms/main.tf
@@ -1,41 +1,40 @@
 module "netbox_vms" {
-  for_each = { for vm in var.vms : vm.name => vm }
-
   source = "../netbox_vms"
 
-  vm_name      = each.value.name
-  memory_mb    = each.value.memory
-  vcpus        = each.value.cpu.cores
-  disk_size_mb = each.value.scsi_disk.size_mb
+  count = length(var.vms)
 
-  extra_disks = each.value.scsi_extra_disks
+  vm_name      = var.vms[count.index].name
+  memory_mb    = var.vms[count.index].memory
+  vcpus        = var.vms[count.index].cpu.cores
+  disk_size_mb = var.vms[count.index].scsi_disk.size_mb
 
-
+  extra_disks  = var.vms[count.index].scsi_extra_disks
   prefix       = var.prefix
   cluster_name = var.cluster_name
   netbox_url   = var.netbox_url
   netbox_token = var.netbox_token
+
 }
 
 module "proxmox_vms" {
-  for_each = { for vm in var.vms : vm.name => vm }
+  source = "../proxmox_vms"
+  count  = length(var.vms)
 
-  source      = "../proxmox_vms"
   vm_user     = var.vm_user
   vm_ssh_keys = var.vm_ssh_keys
 
-  vm_memory           = each.value.memory
-  vm_scsi_disk        = each.value.scsi_disk
-  vm_ip               = module.netbox_vms[each.value.name].ip_address
-  vm_name             = each.value.name
-  vm_state            = each.value.vm_state
-  vm_gateway          = each.value.gateway
-  vm_scsi_extra_disks = each.value.scsi_extra_disks
+  vm_memory           = var.vms[count.index].memory
+  vm_scsi_disk        = var.vms[count.index].scsi_disk
+  vm_ip               = module.netbox_vms[count.index].ip_address
+  vm_name             = var.vms[count.index].name
+  vm_state            = var.vms[count.index].vm_state
+  vm_gateway          = var.vms[count.index].gateway
+  vm_scsi_extra_disks = var.vms[count.index].scsi_extra_disks
 
-  vm_id          = each.value.vmid
-  vm_cpu         = each.value.cpu
-  vm_clone       = each.value.clone
-  vm_tags        = each.value.tags
-  vm_target_node = each.value.target_node
+  vm_id          = var.vms[count.index].vmid
+  vm_cpu         = var.vms[count.index].cpu
+  vm_clone       = var.vms[count.index].clone
+  vm_tags        = var.vms[count.index].tags
+  vm_target_node = var.vms[count.index].target_node
 
 }
