173669ab26f9964a1c6cca63508281bbb9655a03
rene <rene.ruprecht.396@gmail.com>
2025-08-10 15:58:22 +0200

    refactor: terraform will now create just the vm

--- terraform/environments/redstone/netbox/main.tf
+++ terraform/environments/redstone/netbox/main.tf
@@ -1,13 +1,24 @@
-module "netbox_proxmox_vms" {
-  source = "../../../modules/netbox_proxmox_vms"
+module "proxmox_vms" {
+  source = "../../../modules/proxmox_vms"
 
-  vm_user      = var.vm_user
-  vm_ssh_keys  = var.vm_ssh_keys
-  netbox_url   = var.netbox_url
-  netbox_token = var.netbox_token
+  count  = length(var.vms)
 
-  prefix       = "192.168.178.0/24"
-  cluster_name = "Proxmox PVE-02"
-  vms          = var.vms
+  vm_user     = var.vm_user
+  vm_ssh_keys = var.vm_ssh_keys
+
+  vm_memory           = var.vms[count.index].memory
+  vm_scsi_disk        = var.vms[count.index].scsi_disk
+  vm_ip               = var.vms[count.index].ip
+  vm_name             = var.vms[count.index].name
+  vm_state            = var.vms[count.index].vm_state
+  vm_gateway          = var.vms[count.index].gateway
+  vm_scsi_extra_disks = var.vms[count.index].scsi_extra_disks
+
+  vm_id          = var.vms[count.index].vmid
+  vm_cpu         = var.vms[count.index].cpu
+  vm_clone       = var.vms[count.index].clone
+  vm_tags        = var.vms[count.index].tags
+  vm_target_node = var.vms[count.index].target_node
+
 }
 
