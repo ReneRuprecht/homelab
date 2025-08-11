37478761dca88a37d252ba973bed3df6c9671861
rene <rene.ruprecht.396@gmail.com>
2025-08-11 10:17:33 +0200

    feat(terraform): add consul servers for terraform state management

---
+++ terraform/environments/redstone/consul/variables.tf
@@ -0,0 +1,46 @@
+variable "vm_user" {
+  description = "VM User"
+  type        = string
+}
+
+variable "vm_ssh_keys" {
+  description = "VM SSH Keys for login"
+  type        = string
+}
+
+variable "vms" {
+  description = "Liste von VMs mit Parametern für Proxmox"
+  type = list(object({
+    name        = string
+    target_node = optional(string, "pve-02")
+    vmid        = optional(number)
+    memory      = number
+    ip          = optional(string, "")
+    gateway     = string
+    cpu = object({
+      cores = optional(number, 1)
+    })
+    vm_state = string
+    tags     = optional(string)
+    clone    = optional(string, "debian-12-cloud")
+
+    scsi_disk = optional(object({
+      size    = string
+      size_mb = number
+      storage = optional(string, "internal-storage")
+      format  = optional(string, "raw")
+      }), {
+      size    = "20G"
+      size_mb = "20000"
+    })
+
+    scsi_extra_disks = optional(list(object({
+      name    = string
+      size    = string
+      size_mb = number
+      storage = optional(string, "internal-storage")
+      format  = optional(string, "raw")
+      slot    = string
+    })), [])
+  }))
+}
