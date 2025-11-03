e65df05de042ae05afe9e79d750f8472eed1bdad
rene <rene.ruprecht.396@gmail.com>
2025-11-03 19:15:34 +0100

    feat(monitoring): add monitoring terraform and ansible
    grafana, loki, prometheus, promtail, exporter

---
+++ terraform/environments/redstone/monitoring/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "consul" {
+    path    = "terraform/state/redstone/monitoring"
+  }
+
+  required_providers {
+    netbox = {
+      source = "e-breuninger/netbox"
+      version = "4.1.0"
+    }
+    proxmox = {
+      source = "telmate/proxmox"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_tls_insecure = true
+  pm_debug        = true
+}
