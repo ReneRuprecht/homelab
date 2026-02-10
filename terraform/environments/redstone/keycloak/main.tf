f09816ec1cb8146ca9f6db205cd632821973d773
rene <rene.ruprecht.396@gmail.com>
2026-02-11 00:12:58 +0100

    feat(terraform): add keycloak-01 vm

---
+++ terraform/environments/redstone/keycloak/main.tf
@@ -0,0 +1,9 @@
+module "netbox_proxmox_vms" {
+  source = "../../../modules/netbox_proxmox_vms"
+
+  vm_user      = var.vm_user
+  vm_ssh_keys  = var.vm_ssh_keys
+
+  vms          = var.vms
+}
+
---
+++ terraform/environments/redstone/keycloak/terraform.tfvars
@@ -0,0 +1,9 @@
+vms = [
+  {
+    name     = "keycloak-01.redstone"
+    vm_state = "started"
+    memory   = 2048
+    gateway  = "192.168.178.1"
+    cpu      = { cores = 2 }
+  }
+]
