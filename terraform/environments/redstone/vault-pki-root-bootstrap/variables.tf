a261b37da2ee5c3b2172a71e70fe93f7426e59e2
rene <rene.ruprecht.396@gmail.com>
2026-07-10 23:48:19 +0200

    chore(terraform): add vault root ca

--- terraform/environments/redstone/vault-pki-root-bootstrap/variables.tf
+++
@@ -1,9 +0,0 @@
-# variable "vault_address" {
-#   description = "Vault ADDR"
-#   type        = string
-# }
-# variable "vault_token" {
-#   description = "Vault Token"
-#   type        = string
-#   sensitive   = true
-# }
