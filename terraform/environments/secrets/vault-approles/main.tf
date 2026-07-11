0143bb5812e23093ba8489495c6f8553d634a911
rene <rene.ruprecht.396@gmail.com>
2026-07-12 01:18:36 +0200

    feat(terraform): add prometheus-metrics approle

--- terraform/environments/secrets/vault-approles/main.tf
+++ terraform/environments/secrets/vault-approles/main.tf
@@ -10,3 +10,16 @@ resource "vault_approle_auth_backend_role" "haproxy" {
   token_ttl     = 3600
   token_max_ttl = 14400
 }
+
+resource "vault_approle_auth_backend_role" "prometheus-metrics" {
+  backend = "approle"
+
+  role_name = "prometheus-metrics"
+
+  token_policies = [
+    "prometheus-metrics"
+  ]
+
+  token_ttl     = 3600
+  token_max_ttl = 14400
+}
