d5a6c214d26d667a0bcfaff6dd70bf2e39ccb54e
rene <rene.ruprecht.396@gmail.com>
2026-07-12 01:07:37 +0200

    feat(terraform): add prometheus-metrics vault policy

--- terraform/environments/secrets/vault-policies/main.tf
+++ terraform/environments/secrets/vault-policies/main.tf
@@ -1,4 +1,9 @@
-resource "vault_policy" "backup" {
+resource "vault_policy" "haproxy-cert-reader" {
   name   = "haproxy-cert-reader"
   policy = file("${path.module}/policies/haproxy-cert-reader.hcl")
+}
+
+resource "vault_policy" "prometheus-metrics" {
+  name   = "prometheus-metrics"
+  policy = file("${path.module}/policies/prometheus-metrics.hcl")
 }
