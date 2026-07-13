b4be8c1fc1839facaaa68f74604fbf8f4303e5ea
rene <rene.ruprecht.396@gmail.com>
2026-07-13 16:53:51 +0200

    feat(terraform): add cert-manager to vault auth role

--- terraform/environments/secrets/vault-auth-roles/main.tf
+++ terraform/environments/secrets/vault-auth-roles/main.tf
@@ -17,3 +17,16 @@ resource "vault_kubernetes_auth_backend_role" "external_secrets_role" {
   token_ttl                        = 3600
 }
 
+####
+# Cert-Manager
+####
+
+resource "vault_kubernetes_auth_backend_role" "cert_manager_role" {
+  backend                          = "kubernetes"
+  role_name                        = "cert-manager"
+  bound_service_account_names      = ["vault-issuer"]
+  bound_service_account_namespaces = ["cert-manager"]
+  token_policies                   = ["sign-pki-int-redstone", "sign-pki-int-bluecore"]
+  token_ttl                        = 3600
+}
+
