a3ca90d2bd6e124953b7aebf720cbd02e72ea129
rene <rene.ruprecht.396@gmail.com>
2026-07-13 16:56:17 +0200

    feat(terraform): add redstone and bluecore vault pki roles

--- terraform/environments/secrets/vault-pki-intermediates/main.tf
+++ terraform/environments/secrets/vault-pki-intermediates/main.tf
@@ -90,3 +90,63 @@ resource "vault_pki_secret_backend_intermediate_set_signed" "secrets_signed" {
   certificate = resource.vault_pki_secret_backend_root_sign_intermediate.secrets.certificate
 }
 
+
+### 
+# Redstone
+###
+resource "vault_mount" "redstone" {
+  path                  = "pki-int-redstone"
+  type                  = "pki"
+  max_lease_ttl_seconds = 157680000
+  description           = "Homelab Redstone Intermediate Certificate Authority"
+}
+
+resource "vault_pki_secret_backend_intermediate_cert_request" "redstone_csr" {
+  backend     = vault_mount.redstone.path
+  common_name = "Homelab Redstone Intermediate CA"
+  type        = "internal"
+}
+
+resource "vault_pki_secret_backend_root_sign_intermediate" "redstone" {
+  depends_on  = [vault_pki_secret_backend_intermediate_cert_request.redstone_csr]
+  backend     = "pki-root"
+  csr         = vault_pki_secret_backend_intermediate_cert_request.redstone_csr.csr
+  common_name = "Homelab Redstone Intermediate CA"
+
+  ttl = 157680000
+}
+
+resource "vault_pki_secret_backend_intermediate_set_signed" "redstone_signed" {
+  backend     = vault_mount.redstone.path
+  certificate = resource.vault_pki_secret_backend_root_sign_intermediate.redstone.certificate
+}
+
+### 
+# Bluecore
+###
+resource "vault_mount" "bluecore" {
+  path                  = "pki-int-bluecore"
+  type                  = "pki"
+  max_lease_ttl_seconds = 157680000
+  description           = "Homelab Bluecore Intermediate Certificate Authority"
+}
+
+resource "vault_pki_secret_backend_intermediate_cert_request" "bluecore_csr" {
+  backend     = vault_mount.bluecore.path
+  common_name = "Homelab Bluecore Intermediate CA"
+  type        = "internal"
+}
+
+resource "vault_pki_secret_backend_root_sign_intermediate" "bluecore" {
+  depends_on  = [vault_pki_secret_backend_intermediate_cert_request.bluecore_csr]
+  backend     = "pki-root"
+  csr         = vault_pki_secret_backend_intermediate_cert_request.bluecore_csr.csr
+  common_name = "Homelab Bluecore Intermediate CA"
+
+  ttl = 157680000
+}
+
+resource "vault_pki_secret_backend_intermediate_set_signed" "bluecore_signed" {
+  backend     = vault_mount.bluecore.path
+  certificate = resource.vault_pki_secret_backend_root_sign_intermediate.bluecore.certificate
+}
