4d145f4338d691ce183adb5df863d45d105914fe
rene <rene.ruprecht.396@gmail.com>
2026-07-13 16:16:35 +0200

    feat(terraform): add kubernetes auth to vault

--- terraform/environments/secrets/vault-auth/main.tf
+++ terraform/environments/secrets/vault-auth/main.tf
@@ -1,3 +1,16 @@
 resource "vault_auth_backend" "approle" {
   type = "approle"
 }
+
+resource "vault_auth_backend" "k8s" {
+  type = "kubernetes"
+  path = "kubernetes"
+}
+
+resource "vault_kubernetes_auth_backend_config" "k8s" {
+  depends_on           = [vault_auth_backend.k8s]
+  backend              = vault_auth_backend.k8s.path
+  kubernetes_host      = var.k8s_host
+  kubernetes_ca_cert   = base64decode(var.k8s_ca_cert)
+  disable_local_ca_jwt = true
+}
--- terraform/environments/secrets/vault-auth/variables.tf
+++ terraform/environments/secrets/vault-auth/variables.tf
@@ -7,3 +7,14 @@ variable "vault_token" {
   type        = string
   sensitive   = true
 }
+
+variable "k8s_ca_cert" {
+  description = "Kubernetes cluster CA certificate (Base64 encoded)"
+  type        = string
+  sensitive   = true
+}
+variable "k8s_host" {
+  description = "Kubernetes host"
+  type        = string
+  sensitive   = true
+}
